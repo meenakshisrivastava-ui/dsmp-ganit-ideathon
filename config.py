d52b347442d17e03583b62aea191bd16876346a2
meenakshisrivastava-ui <meenakshi.srivastava@ganitinc.com>
2026-06-08 17:22:22 +0530

    v2

---
+++ config.py
@@ -0,0 +1,17 @@
+# ── MailHog ───────────────────────────────────────────
+MAILHOG_API   = "http://localhost:8025/api/v2/messages"
+MAILHOG_LIMIT = 50
+
+# ── Mattermost ────────────────────────────────────────
+MM_BASE_URL = "http://localhost:8065/api/v4"
+MM_USERNAME = "MeenakshiSri"
+MM_PASSWORD = "Srime220197"
+
+# ── AWS S3 ────────────────────────────────────────────
+AWS_ACCESS_KEY    = "AKIA3YAD5XW5QX5TBKIB"
+AWS_SECRET_KEY    = "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"
+AWS_REGION        = "ap-south-1"
+
+# ── S3 Bucket Names ───────────────────────────────────
+BUCKET_EMAIL = "dsmp-email-metadata"
+BUCKET_CHAT  = "dsmp-chat-metadata"
